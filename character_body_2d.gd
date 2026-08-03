e1818819cd3cb2d7eb0530909450791aea0b8980
danylokostiuk <danylokostiukhuk@gmail.com>
2026-08-03 19:48:52 +0200

    Animations fixed
    changed code a lil so the animation would work

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,24 +1,18 @@
 extends CharacterBody2D
-@onready var play_anim = $AnimatedSprite2D
+
 var speed = 300
 var force = 400
 var poids = 900
 var hp = 3
 var is_onground = true
 var jump = 0
+
+@onready var play_anim = $AnimatedSprite2D
 @onready var txt_jump = $jump
-
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	txt_jump.text = str(jump)
-	
-	
-	
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -36,25 +30,21 @@ func _physics_process(delta: float) -> void:
 			velocity.x = speed
 			play_anim.play("dash")
 			play_anim.flip_h = false
+			
 		if Input.is_action_pressed("rec"):
 			velocity.x = -speed
 			play_anim.play("dash")
 			play_anim.flip_h = true
+			
 		if  Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y -= force
 			play_anim.play("jump")
 			jump += 1
-		elif Input.is_action_just_pressed("atk"):
+			
+		if Input.is_action_just_pressed("atk"):
 			play_anim.play("atk")
 			
-			
-		if velocity.x == 0:
+		elif velocity.x == 0 and velocity.y == 0 and !(play_anim.animation == "atk"):
 			play_anim.play("idle")
 			
-			
-		
 	move_and_slide()
-	
-	
-	
-	pass

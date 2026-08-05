51efea7ecb7a40d6f245a2ddcdf45ebbc885692f
danylokostiuk <danylokostiukhuk@gmail.com>
2026-08-05 23:29:27 +0200

    sorted all out

--- platform/scripts/character_body_2d.gd
+++ platform/scripts/character_body_2d.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
-
+@onready var play_anim = $AnimatedSprite2D
 var speed = 300
 var force = 400
 var poids = 900
 var hp = 3
 var is_onground = true
 var jump = 0
-
-@onready var play_anim = $AnimatedSprite2D
 @onready var txt_jump = $jump
+var global
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	txt_jump.text = str(jump)
+	
+	
+	
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -28,25 +35,27 @@ func _physics_process(delta: float) -> void:
 	if hp > 0:
 		if Input.is_action_pressed("av"):
 			velocity.x = speed
-			if is_on_floor():
-				play_anim.play("dash")
+			play_anim.play("dash")
 			play_anim.flip_h = false
-			
 		if Input.is_action_pressed("rec"):
 			velocity.x = -speed
-			if is_on_floor():
-				play_anim.play("dash")
+			play_anim.play("dash")
 			play_anim.flip_h = true
-			
 		if  Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y -= force
 			play_anim.play("jump")
 			jump += 1
-			
-		if Input.is_action_just_pressed("atk"):
+		elif Input.is_action_just_pressed("atk"):
 			play_anim.play("atk")
 			
-		elif velocity.x == 0 and velocity.y == 0 and !(play_anim.animation == "atk"):
+			
+		if velocity.x == 0:
 			play_anim.play("idle")
 			
+			
+		
 	move_and_slide()
+	
+	
+	
+	pass
